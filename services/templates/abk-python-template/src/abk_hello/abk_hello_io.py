abfdb9fc1ac4dbb2a69076b2600cf2cfdd630863
Alex Berger <alexbigkid@users.noreply.github.com>
2025-06-17 03:26:30 -0700

    e updated python template

--- services/templates/abk-python-template/src/abk_hello/abk_hello_io.py
+++ services/templates/abk-python-template/src/abk_hello/abk_hello_io.py
@@ -1,33 +1,39 @@
-"""Lambda In / Out - Request / Response definitions"""
-from typing import NamedTuple, Optional
+"""Lambda In / Out - Request / Response definitions."""
+
+from typing import NamedTuple
 
 
 class AhLambdaRequestBody(NamedTuple):
-    """Class to store lambda request
+    """Class to store lambda request.
+
     An example of the lambda request would be:
     {
         "deviceUuid": "15a73c3e-0c86-495a-aa2b-522691d93d60",
         "txId": "any_thing_with_length_upto_36_chars",
     }
     """
-    deviceUuid: str         # required / should not be empty
-    txId: str               # required / should not be empty
+
+    deviceUuid: str  # required / should not be empty
+    txId: str  # required / should not be empty
 
 
 class AhLambdaResponseBody(NamedTuple):
-    """Class to store lambda response body information
+    """Class to store lambda response body information.
+
     An example of the lambda response body would be:
     {
         "msg": "OK",
         "txId": "any_thing_with_length_upto_36_chars"
     }
     """
+
     msg: str
     txId: str
 
 
 class AhLambdaResponse(NamedTuple):
-    """Class to store lambda response
+    """Class to store lambda response.
+
     An example of the lambda response would be:
     {
         "statusCode": 200,
@@ -39,6 +45,7 @@ class AhLambdaResponse(NamedTuple):
         "body": json.dumps(lambdaResponseBody)
     }
     """
+
     statusCode: int
-    headers: Optional[dict]
+    headers: dict | None
     body: str
