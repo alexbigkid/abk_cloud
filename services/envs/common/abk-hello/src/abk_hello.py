bb08ab4470d27886128d23461e392b70c7713d5d
Alex Berger <alexbigkid@users.noreply.github.com>
2025-06-17 02:02:06 -0700

    e uv changes

--- services/envs/common/abk-hello/src/abk_hello.py
+++ services/envs/common/abk-hello/src/abk_hello.py
@@ -1,17 +1,16 @@
-"""Provides lambda functionality in ABK cloud infrastructure"""
+"""Provides lambda functionality in ABK cloud infrastructure."""
 
 # Standard imports
 import json
 import logging
 import os
-import sys
 from enum import Enum
 
 # 3rd party imports
 from jsonschema import validate
 
 # local imports
-from abk_hello_io import AhLambdaRequestBody, AhLambdaResponseBody
+from .abk_hello_io import AhLambdaRequestBody, AhLambdaResponseBody
 
 # -----------------------------------------------------------------------------
 # variables definitions, file wide access, for lambda to load only once.
@@ -34,23 +33,26 @@ LAMBDA_REQ_SCHEMA = {
     "title": "ABK Lambda Request Validation",
     "description": "JSON Schema validation for ABK hello get Lambda Request.",
     "$defs": {
-        "uuid": {"type": "string", "pattern": "^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$", "minLength": 36, "maxLength": 36}
+        "uuid": {
+            "type": "string",
+            "pattern": "^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$",
+            "minLength": 36,
+            "maxLength": 36,
+        }
     },
-
     "type": "object",
     "properties": {
-        "deviceUuid":   {"$ref": "#/$defs/uuid"},
-        "txId":         {"type": "string", "minLength": 1, "maxLength": 36}
+        "deviceUuid": {"$ref": "#/$defs/uuid"},
+        "txId": {"type": "string", "minLength": 1, "maxLength": 36},
     },
-    "required": [
-        "deviceUuid",
-        "txId"
-    ],
-    "additionalProperties": False
+    "required": ["deviceUuid", "txId"],
+    "additionalProperties": False,
 }
 
+
 class HttpStatusCode(Enum):
-    """HTTP status codes used in this lambda"""
+    """HTTP status codes used in this lambda."""
+
     OK = 200
     FORBIDDEN = 403
     CONFLICT = 409
@@ -59,8 +61,9 @@ class HttpStatusCode(Enum):
 # -----------------------------------------------------------------------------
 # local functions
 # -----------------------------------------------------------------------------
-def validate_input(input_parameters:dict) -> AhLambdaRequestBody:
-    """Validates and converts input parameters
+def validate_input(input_parameters: dict) -> AhLambdaRequestBody:
+    """Validates and converts input parameters.
+
     Args:
         input_parameters (dict[str, str]): lambda input parameter dict
     Raises:
@@ -72,24 +75,23 @@ def validate_input(input_parameters:dict) -> AhLambdaRequestBody:
     return AhLambdaRequestBody(**input_parameters)
 
 
-def get_error_response_body(event_body:dict) -> AhLambdaResponseBody:
-    """Constructs lambda response body in error case
+def get_error_response_body(event_body: dict) -> AhLambdaResponseBody:
+    """Constructs lambda response body in error case.
+
     Args:
         event_body (dict): lambda event
     Returns:
         LambdaResponseBody: body response
     """
-    abk_logger.info(f"-> get_error_response_body()")
-    resp_body = AhLambdaResponseBody(
-        msg="error",
-        txId=event_body.get('txId', '')
-    )
+    abk_logger.info("-> get_error_response_body()")
+    resp_body = AhLambdaResponseBody(msg="error", txId=event_body.get("txId", ""))
     abk_logger.info(f"<- get_error_response_body({json.dumps(resp_body._asdict(), indent=4)})")
     return resp_body
 
 
 def class_to_dict(named_tuple) -> object:
-    """ Converts data class or NamedTuple object to dict recursively.
+    """Converts data class or NamedTuple object to dict recursively.
+
     Args:
         named_tuple: named tuple
     Returns:
@@ -104,37 +106,36 @@ def class_to_dict(named_tuple) -> object:
     return named_tuple
 
 
-
 # -----------------------------------------------------------------------------
 # lambda handler - main function
 # -----------------------------------------------------------------------------
 def handler(event, context):
     """Handler for removing device from the ABK device table.
+
     Args:
         event (dict): event data dictionary
         context (object): lambda context object
     Returns:
         http_resp dict: lambda response dictionary, where body is a string converted from dict
     """
-    status_code = HttpStatusCode.FORBIDDEN.value # Assume error at the beginning, overwrite alter
+    status_code = HttpStatusCode.FORBIDDEN.value  # Assume error at the beginning, overwrite alter
     abk_logger.info(f"event   = {json.dumps(event, indent=2)}")
-    abk_logger.debug(f"context = {json.dumps(context, default=lambda o: getattr(o, '__dict__', str(o)))}")
+    abk_logger.debug(
+        f"context = {json.dumps(context, default=lambda o: getattr(o, '__dict__', str(o)))}"
+    )
     resp_body: AhLambdaResponseBody
 
     try:
-        lambda_input = json.loads(event.get('body'))
+        lambda_input = json.loads(event.get("body"))
         lambda_req = validate_input(lambda_input)
         abk_logger.debug(f"req: {json.dumps(lambda_req._asdict(), indent=4)}")
 
-        resp_body = AhLambdaResponseBody(
-            msg="ok",
-            txId=lambda_req.txId,
-        )
+        resp_body = AhLambdaResponseBody(msg="ok", txId=lambda_req.txId)
 
         status_code = HttpStatusCode.OK.value
     except Exception as exc:
         abk_logger.error(f"{exc = }")
-        resp_body = get_error_response_body(json.loads(event.get('body')))
+        resp_body = get_error_response_body(json.loads(event.get("body")))
 
     body = json.dumps(class_to_dict(resp_body))
     abk_logger.info(f"{status_code = }, {body = }")
